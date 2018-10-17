50b852cf8ee84acf9fa93e065b48be31dd782b92
Tim Head <betatim@gmail.com>
2018-10-17 22:41:11 +0200

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 
+import multiprocessing
 
 def compute(n):
     a = 'b' * 1 * (1024 * 1024 * 1024)
@@ -7,6 +8,7 @@ def compute(n):
         x = 42/3.
 
 def main():
+    print('cores', multiprocessing.cpu_count())
     with concurrent.futures.ProcessPoolExecutor(20) as executor:
         for prime in zip(range(40), executor.map(compute, range(40))):
             print('%d is prime: %s' % (prime, prime))
