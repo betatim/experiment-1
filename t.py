85d70eab22512d2129ff504ba1ccbad7da561182
Tim Head <betatim@gmail.com>
2018-10-17 22:39:52 +0200

    Update t.py

--- t.py
+++ t.py
@@ -1,13 +1,15 @@
 import concurrent.futures
 
+
 def compute(n):
+    a = 'b' * 1 * (1024 * 1024 * 1024)
     while True:
         x = 42/3.
 
 def main():
-    with concurrent.futures.ProcessPoolExecutor(10) as executor:
-        for number, prime in zip(range(20), executor.map(compute, range(20))):
-            print('%d is prime: %s' % (number, prime))
+    with concurrent.futures.ProcessPoolExecutor(20) as executor:
+        for prime in zip(range(40), executor.map(compute, range(40))):
+            print('%d is prime: %s' % (prime, prime))
 
 if __name__ == '__main__':
     main()
